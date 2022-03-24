3766ba7291bebcc3dc24c8d7d642a09a7007aa57
Abhishek Raj <abhishekraj1130@gmail.com>
2022-03-24 22:56:53 +0530

    test check

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,22 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/edit/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders characters', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn/i);
+  expect(linkElement).toBeInTheDocument();
+});
+test('renders numbers', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/1/i);
+  expect(linkElement).toBeInTheDocument();
+});
+test('renders special characters', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/#/i);
   expect(linkElement).toBeInTheDocument();
 });
